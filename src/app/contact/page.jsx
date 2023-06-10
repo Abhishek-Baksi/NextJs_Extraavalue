f8164618b93fba91784b936aabdb20fab964b342
abhishek-baksi <abhisek.baksi@icloud.com>
2023-06-10 14:26:58 +0530

    styles and animations addes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 function Contact() {
   return (
     <div className={styles.container}>
-        Contact
+      <div className={styles.title}>
+        <h1>Lets. keep in touch</h1>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="text" placeholder="Email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="Message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contact;
