6f1b9e8bbbbda045236e5cabc7906d1df8708c7b
abhishek-baksi <abhisek.baksi@icloud.com>
2023-06-11 07:13:10 +0530

    1- simple-react-typewriiter installed 2-Home have modifiend to ssr to csr 3-one css added 4- if some issue occurs then remove them

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,42 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "public/hero.png";
 import Button from "@/components/Button/Button";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Best Place to sell your Old Smart Phone!</h1>
-        <p className={styles.desc}>Grab the best deal on your old smartphone!</p>
+        <h1 className={styles.title}>
+          Best Place to sell your Old Smart Phone!
+        </h1>
+        <p className={styles.desc}>
+          Grab the best deal on your old smartphone!
+          <br />
+          {/* this is added remove the span typewriter-container and use-client and br */}
+          <span style={{ color: "#53c28b", fontWeight: "bold" }}>
+            <span
+              style={{ color: "white", fontSize: "20px", fontWeight: "normal" }}
+            >
+              Instant
+            </span>{" "}
+            <Typewriter
+              words={["Payment", "Price", "Pickup"]}
+              loop={true}
+              delayBetweenLetters={75}
+              delayBetweenWords={1000}
+            />
+            <span>|</span>
+          </span>
+        </p>
         <Button url="/portpolio" text="See our Service" />
       </div>
-      
+
       <div className={styles.item}>
-      <Image src={Hero} alt="" className={styles.img}></Image>
+        <Image src={Hero} alt="" className={styles.img}></Image>
       </div>
-      
     </div>
   );
 }
