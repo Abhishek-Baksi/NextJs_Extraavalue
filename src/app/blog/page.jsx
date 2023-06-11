71d4e3a2d8a35dc33dc1f8563c6298e14bb73582
abhishek-baksi <abhisek.baksi@icloud.com>
2023-06-11 06:22:29 +0530

    about and blog page updated

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 function Blog() {
   return (
-    <div>
-        Blog
+    <div className={styles.mainContainer}>
+        <Link className={styles.container} href="/blog/testId">
+        <div className={styles.imageContainer}> 
+        <Image className={styles.img} src="https://images.pexels.com/photos/6913718/pexels-photo-6913718.jpeg?auto=compress&cs=tinysrgb&w=600" alt='' width={400} height={250}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>TEST</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+        </Link>
+
+        <Link className={styles.container} href="/blog/testId">
+        <div className={styles.imageContainer}> 
+        <Image className={styles.img} src="https://images.pexels.com/photos/6913718/pexels-photo-6913718.jpeg?auto=compress&cs=tinysrgb&w=600" alt='' width={400} height={250}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>TEST</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+        </Link>
     </div>
   )
 }
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import styles from './page.module.css'
-function Category({params}) {
-  console.log(params)
+import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
+
+function Category({ params }) {
+  console.log(params);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Get Best Price</h1>
+          <p className={styles.desc}>
+            Enter your phone details.Get instant buyback offer.Download App and
+            complete diagnostics.Seal the deal!
+          </p>
+          <Button text={"see more"} url={"#"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/6913718/pexels-photo-6913718.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Get Best Price</h1>
+          <p className={styles.desc}>
+            Enter your phone details.Get instant buyback offer.Download App and
+            complete diagnostics.Seal the deal!
+          </p>
+          <Button text={"see more"} url={"#"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/6913718/pexels-photo-6913718.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
