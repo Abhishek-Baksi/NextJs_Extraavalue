526dda0bf923ef0f482b26e366f0f21249b6fcf1
abhishek-baksi <abhisek.baksi@icloud.com>
2023-06-11 22:17:40 +0530

    Links Tile changes from portfolio to sell now

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,64 @@
-'use client'
-import React from 'react'
-import Link from 'next/link'
-import styles from "./navbar.module.css"
-import DarkModeToogle from '../DarkModeToogle/DarkModeToogle';
+"use client";
+import React from "react";
+import Link from "next/link";
+import styles from "./navbar.module.css";
+import DarkModeToogle from "../DarkModeToogle/DarkModeToogle";
 
 const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "Contact",
-      url: "/contact",
-    },
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Sell Now",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About Us",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 function Navbar() {
   return (
     <div className={styles.container}>
-        <Link href="/" className={styles.logo}>Extraavalue DEMO</Link>
-        <div className={styles.links}>
-          <DarkModeToogle/>
-            {
-                links.map((link)=> <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>)
-            }
-        </div>
-        <button className={styles.logout} onClick={()=>console.log("Logged Out")}>Logout</button>
+      <Link href="/" className={styles.logo}>
+        Extraavalue
+      </Link>
+      <div className={styles.links}>
+        <DarkModeToogle />
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+      <button
+        className={styles.logout}
+        onClick={() => console.log("Logged Out")}
+      >
+        Logout
+      </button>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
